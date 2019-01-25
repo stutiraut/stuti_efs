c33b494fbbecca978d0e9d6f612f19f0d8d046f7
stutiraut <sraut@unomaha.edu>
2019-01-25 15:15:23 -0600

    Added urls.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
-# Create your views here.
+
+def customer_list(request):
+    customer = Customer.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'portfolio/customer_list.html',
+                 {'customers': customer})
