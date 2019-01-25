5af452d27e7ffc3549d6d2f0c3357a94caa088de
stutiraut <sraut@unomaha.edu>
2019-01-25 13:55:07 -0600

    Initial commit

---
+++ portfolio/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Customer
+
+
+class CustomerList(admin.ModelAdmin):
+    list_display = ('cust_number', 'name', 'city', 'cell_phone')
+    list_filter = ('cust_number', 'name', 'city')
+    search_fields = ('cust_number', 'name')
+    ordering = ['cust_number']
+
+
+
+admin.site.register(Customer)
